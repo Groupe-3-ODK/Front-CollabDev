e90aab0355ff0f9bc61a83fd98ac06179bb0e93e
Mohamed Toure <berrahtech1995@gmail.com>
2025-08-05 14:33:31 +0000

    mise à jour de dashboard

--- collab-dev/src/app/app.component.ts
+++ collab-dev/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfilComponent } from '../pages/users/profil/profil.component';
+import { DashboardComponent } from '../pages/users/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ProfilComponent ],
+  imports: [DashboardComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
