7fb6fed7b41e3a881bd4d8f7182b4b9c040c615e
Aissatou Coulibaly <coulibalyaissatou37@gmail.com>
2025-08-05 17:32:57 +0200

    siadbar-user

--- collab-dev/src/app/app.component.ts
+++ collab-dev/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { UserSidebarComponent } from './user-sidebar/user-sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    UserSidebarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- collab-dev/src/app/app.routes.ts
+++ collab-dev/src/app/app.routes.ts
@@ -81,6 +81,8 @@ export const routes: Routes = [
             '../pages/admin/confidentiality-policy/confidentiality-policy.component'
           ).then((m) => m.ConfidentialityPolicyComponent),
       },
+      //-----------------------------------------------------------------------
+      
     ],
   },
 ];
