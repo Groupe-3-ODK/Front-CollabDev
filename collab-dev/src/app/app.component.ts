5533ee8de8b7fc1cc8492873f703636ea01ddc65
Mohamed Toure <berrahtech1995@gmail.com>
2025-08-05 17:41:30 +0000

    third update projecttodo

--- collab-dev/src/app/app.component.ts
+++ collab-dev/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfilComponent } from '../pages/users/profil/profil.component';
 import { DashboardComponent } from '../pages/users/dashboard/dashboard.component';
+import { ProjecttodoComponent } from '../pages/users/projecttodo/projecttodo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [DashboardComponent ],
+  imports: [ProjecttodoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- collab-dev/src/pages/users/projecttodo/projecttodo.component.ts
+++ collab-dev/src/pages/users/projecttodo/projecttodo.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../../shared/reusablesComponents/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-projecttodo',
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './projecttodo.component.html',
   styleUrl: './projecttodo.component.css'
 })
