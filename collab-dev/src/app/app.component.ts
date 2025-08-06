64149edafb58c1ae848fac69c0cdc0a5b724f640
Sekou <sekouk1410@gmail.com>
2025-08-06 22:31:41 +0000

    feat(signup) : Ajout de la page d'inscription

--- collab-dev/src/app/app.component.ts
+++ collab-dev/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { ProjectDoneComponent } from '../pages/users/project-done/project-done.component';
+import { SignupComponent } from '../components/signup/signup';
 
 @Component({
   selector: 'app-root',
-  imports: [ProjectDoneComponent],
+  standalone: true,
+  imports: [ProjectDoneComponent, SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
