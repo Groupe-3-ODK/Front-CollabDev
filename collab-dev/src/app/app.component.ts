fa77173923bac4345fcf01627d437109f5b8cc6d
Sekou <sekouk1410@gmail.com>
2025-08-06 08:58:09 +0000

    feat(login) : ajout de la page de connexion

--- collab-dev/src/app/app.component.ts
+++ collab-dev/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { LoginComponent} from '../components/login/login.component';// Importez le composant
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ collab-dev/src/components/login/login.component.ts
@@ -0,0 +1,31 @@
+// login.component.ts
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule,CommonModule], // Importe ReactiveFormsModule pour ce composant
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      rememberMe: [false]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Formulaire soumis', this.loginForm.value);
+      // Ajoutez ici la logique pour envoyer les données au backend
+    }
+  }
+}
