49602f73cd94d58b0840aff5f24b2622b9b2c4b8
Mohamed Toure <berrahtech1995@gmail.com>
2025-08-06 17:31:27 +0000

    création de projectinprogress

--- collab-dev/src/app/app.component.ts
+++ collab-dev/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { ProfilComponent } from '../pages/users/profil/profil.component';
 import { DashboardComponent } from '../pages/users/dashboard/dashboard.component';
 import { ProjectDoneComponent } from '../pages/users/project-done/project-done.component';
+import { ProjectinprogressComponent } from '../pages/users/projectinprogress/projectinprogress.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ProjectDoneComponent ],
+  imports: [ProjectinprogressComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
