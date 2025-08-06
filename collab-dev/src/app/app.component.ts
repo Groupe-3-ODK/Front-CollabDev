85a3c1fe28194fc7e3651aa4bc9b522a877ac844
DOUKING-95 <dembeleseydou053@gmail.com>
2025-08-06 17:36:40 +0000

    commit before switch on projectinprogress

--- collab-dev/src/app/app.component.ts
+++ collab-dev/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from '../components/login/login.component';
 import { ProjectDoneComponent } from '../pages/users/project-done/project-done.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ProjectDoneComponent],
+  imports: [ProjectDoneComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
