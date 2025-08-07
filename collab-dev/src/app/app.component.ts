8d12488ab9e532d2d793dd7c71ac246343635a68
Mohamed Toure <berrahtech1995@gmail.com>
2025-08-07 17:09:29 +0000

    mise à jour finale de detailProjet

--- collab-dev/src/app/app.component.ts
+++ collab-dev/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfilComponent } from '../pages/users/profil/profil.component';
 import { DashboardComponent } from '../pages/users/dashboard/dashboard.component';
+import { DetailProjetComponent } from '../pages/users/detail-projet/detail-projet.component';
 
 @Component({
   selector: 'app-root',
-  imports: [DashboardComponent ],
+  imports: [DetailProjetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
