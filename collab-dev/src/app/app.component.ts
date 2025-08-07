c45d0a9b4b929813b1f81846492649986109f045
DOUKING-95 <dembeleseydou053@gmail.com>
2025-08-07 10:22:24 +0000

    remove projectdone in app import

--- collab-dev/src/app/app.component.ts
+++ collab-dev/src/app/app.component.ts
@@ -1,17 +1,11 @@
 import { Component } from '@angular/core';
 
 import { SignupComponent } from '../components/signup/signup';
-import { ProjectDoneComponent } from '../pages/users/project-done/project-done.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProjectDoneComponent, SignupComponent],
-
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, LandingPage],
+  imports: [SignupComponent],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
