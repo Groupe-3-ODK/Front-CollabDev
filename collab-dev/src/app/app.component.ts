0e4bc40afe3474cea49477c0c60c58b92cc3bf53
Sekou <sekouk1410@gmail.com>
2025-08-07 12:24:40 +0000

    feat(forgot-password) : Ajout de la page forgot password

--- collab-dev/src/app/app.component.ts
+++ collab-dev/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ProjectDoneComponent } from '../pages/users/project-done/project-done.component';
 import { SignupComponent } from '../components/signup/signup';
+import { ForgotPasswordComponent } from '../components/forgot-password/forgot-password';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProjectDoneComponent, SignupComponent],
+  imports: [ProjectDoneComponent, SignupComponent, ForgotPasswordComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
