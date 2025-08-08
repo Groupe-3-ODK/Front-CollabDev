355305238e75f606076261eeb1e69d2814af3518
DOUKING-95 <dembeleseydou053@gmail.com>
2025-08-08 21:29:37 +0000

    ajout du landing page deux et creation du modal pour la creation de tache

--- collab-dev/src/app/app.component.ts
+++ collab-dev/src/app/app.component.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
 
 import { RouterOutlet } from '@angular/router';
-import { LandingPage } from '../components/landing-page/landing-page';
+import { LandingPageTwo } from '../components/landing-page-two/landing-page-two';
+import { CreateTaskModal } from '../shared/reusablesComponents/create-task-modal/create-task-modal';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LandingPage],
+  imports: [RouterOutlet, LandingPageTwo, CreateTaskModal],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'collab-dev';
+
+  // Variable qui contrôle l'affichage du modal
+  isModalVisible: boolean = true;
+
+  // Méthode pour ouvrir le modal
+  openModal(): void {
+    this.isModalVisible = true;
+  }
+
+  // Méthode appelée lorsque le modal émet l'événement 'closeModal'
+  onModalClose(): void {
+    this.isModalVisible = false;
+  }
+
+  // Méthode appelée lorsque le modal émet l'événement 'saveData'
+  onProjectSave(data: any): void {
+    console.log('Données du projet enregistrées:', data);
+    // Ici, vous pouvez ajouter la logique pour traiter les données
+    this.isModalVisible = false;
+  }
 }
