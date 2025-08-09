cf38dddbd14c3346d316171f5ecb887a3012761a
Aissatou Coulibaly <coulibalyaissatou37@gmail.com>
2025-08-09 11:14:49 +0200

    project-form

--- collab-dev/src/app/app.component.ts
+++ collab-dev/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { UserSidebarComponent } from './user-sidebar/user-sidebar.component';
+import { ProjectFormComponent } from '../shared/project-form/project-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    UserSidebarComponent
+    UserSidebarComponent,
+    ProjectFormComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
