0755115fb1555e6fb08ddd7015ad17bef305de3c
elinkalika223 <elinkalika@gmail.com>
2025-08-12 10:19:51 +0000

    Nouvel commit de la pages gestion-users-cote-admin et la page accepter-manager

--- collab-dev/src/app/app.component.ts
+++ collab-dev/src/app/app.component.ts
@@ -5,10 +5,12 @@ import { RouterOutlet } from '@angular/router';
 import { ProfilComponent } from '../pages/users/profil/profil.component';
 import { DashboardComponent } from '../pages/users/dashboard/dashboard.component';
 import { DetailProjetComponent } from '../pages/users/detail-projet/detail-projet.component';
+import { GestionUserCoteAdminComponent } from '../pages/admin/gestion-user-cote-admin/gestion-user-cote-admin.component';
+import { AccepterManager } from '../pages/admin/accepter-manager/accepter-manager';
 
 @Component({
   selector: 'app-root',
-  imports: [DetailProjetComponent],
+  imports: [DetailProjetComponent,GestionUserCoteAdminComponent,AccepterManager],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
---
+++ collab-dev/src/pages/admin/accepter-manager/accepter-manager.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accepter-manager',
+  imports: [],
+  templateUrl: './accepter-manager.html',
+  styleUrl: './accepter-manager.css'
+})
+export class AccepterManager {
+
+}
