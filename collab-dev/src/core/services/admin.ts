8f0120d7aadc30237b7966563db0af50be80e509
Aissatou Coulibaly <coulibalyaissatou37@gmail.com>
2025-08-14 15:20:33 +0200

    service add manager to project

--- collab-dev/src/core/services/admin.ts
+++ collab-dev/src/core/services/admin.ts
@@ -1,49 +1,21 @@
-import { Injectable } from '@angular/core';
-
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { CONSTANT } from '../constants/contant';
+import { environment } from '../../environments/environment';
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class Admin {
-  //   attributeManagerCoins(projectId: number, coins: number): Observable<any> {
-  //   const params = new HttpParams()
-  //     .set('projectId', projectId.toString())
-  //     .set('coins', coins.toString());
-  //   return this.http.put(
-  //     `${this._apiUrl}/attributeCoinsToManager`,
-  //     null, // pas de body
-  //     { params }
-  //   );
-  // }
-  // this.myService.attributeManagerCoins(123, 50.5).subscribe({
-  //   next: res => console.log('Coins attribués', res),
-  //   error: err => console.error('Erreur attribution coins', err)
-  // });
-  //------------------------------------
-  // attributeManagerToProject(projectId: number, managerId: number): Observable<any> {
-  //   const params = new HttpParams()
-  //     .set('projectId', projectId.toString())
-  //     .set('managerId', managerId.toString());
-  //   return this.http.put(
-  //     `${this._apiUrl}/attributeManagerToProject`,
-  //     null, // pas de body
-  //     { params }
-  //   );
-  // }
-  // this.myService.attributeManagerToProject(123, 45).subscribe({
-  //   next: res => console.log('Manager attribué au projet', res),
-  //   error: err => console.error('Erreur attribution manager', err)
-  // });
-  //-----------------------------------
-  // trustProject(projectId: number): Observable<any> {
-  //   const params = new HttpParams().set('projectId', projectId.toString());
-  //   return this.http.put(
-  //     `${this._apiUrl}/trustProject`,
-  //     null, // pas de body
-  //     { params }
-  //   );
-  // }
-  // this.myService.trustProject(123).subscribe({
-  //   next: res => console.log('Projet validé/trusté', res),
-  //   error: err => console.error('Erreur validation projet', err)
-  // });
+   private _http = inject(HttpClient);
+  private apiAdminrUrl =
+    environment.API_BASE_URL + CONSTANT.ADMIN_RESSOURCES.ATTRIBUT_MANAGER;
+  attributeManagerToProject(projectId: number , managerrId:number){
+  const params = new HttpParams()
+  .set('projectId',projectId.toString())
+  .set('managerId',managerrId.toString());
+  return this._http.post(
+      `${this.apiAdminrUrl}/attributeManagerToProject`, 
+      { params }
+    );
+}
 }
